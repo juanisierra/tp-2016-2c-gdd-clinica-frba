5ea591590edf6de31ef091945a7c2600c2def0fc
il mostro di firenze <franco.balduzzi@hotmail.com>
2016-10-31 13:21:50 -0300

    mejoro el login berreta ese

--- src/ClinicaFrba/ClinicaFrba/Login.Designer.cs
+++ src/ClinicaFrba/ClinicaFrba/Login.Designer.cs
@@ -38,39 +38,37 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(83, 53);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 24);
+            this.label1.Size = new System.Drawing.Size(64, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(83, 175);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 97);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 24);
+            this.label2.Size = new System.Drawing.Size(89, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña:";
             // 
             // txtbox_usuario
             // 
-            this.txtbox_usuario.Location = new System.Drawing.Point(208, 58);
-            this.txtbox_usuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtbox_usuario.Location = new System.Drawing.Point(122, 44);
+            this.txtbox_usuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtbox_usuario.Name = "txtbox_usuario";
-            this.txtbox_usuario.Size = new System.Drawing.Size(155, 20);
+            this.txtbox_usuario.Size = new System.Drawing.Size(205, 22);
             this.txtbox_usuario.TabIndex = 2;
             // 
             // btn_iniciar
             // 
-            this.btn_iniciar.Location = new System.Drawing.Point(464, 245);
-            this.btn_iniciar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_iniciar.Location = new System.Drawing.Point(122, 155);
+            this.btn_iniciar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_iniciar.Name = "btn_iniciar";
-            this.btn_iniciar.Size = new System.Drawing.Size(147, 25);
+            this.btn_iniciar.Size = new System.Drawing.Size(158, 35);
             this.btn_iniciar.TabIndex = 4;
             this.btn_iniciar.Text = "Iniciar Sesión";
             this.btn_iniciar.UseVisualStyleBackColor = true;
@@ -78,24 +76,25 @@
             // 
             // txtbox_contrasenia
             // 
-            this.txtbox_contrasenia.Location = new System.Drawing.Point(208, 180);
+            this.txtbox_contrasenia.Location = new System.Drawing.Point(123, 96);
+            this.txtbox_contrasenia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtbox_contrasenia.Name = "txtbox_contrasenia";
-            this.txtbox_contrasenia.Size = new System.Drawing.Size(155, 20);
+            this.txtbox_contrasenia.Size = new System.Drawing.Size(204, 22);
             this.txtbox_contrasenia.TabIndex = 3;
             this.txtbox_contrasenia.UseSystemPasswordChar = true;
             // 
             // Login
             // 
             this.AcceptButton = this.btn_iniciar;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 370);
+            this.ClientSize = new System.Drawing.Size(391, 227);
             this.Controls.Add(this.txtbox_contrasenia);
             this.Controls.Add(this.btn_iniciar);
             this.Controls.Add(this.txtbox_usuario);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.Text = "Clinica-FRBA Login";
             this.Load += new System.EventHandler(this.Login_Load);
